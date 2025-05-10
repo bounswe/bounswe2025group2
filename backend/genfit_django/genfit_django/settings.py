563674e000ab90cb020247698d91e227c280dab4
Berkay <89387602+berkaybgk@users.noreply.github.com>
2025-05-10 20:28:07 +0300

    Update settings.py
    digital ocean ip added

--- backend/genfit_django/genfit_django/settings.py
+++ backend/genfit_django/genfit_django/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = 'django-insecure-p6p*^^1$rp!n(^wo$dqu72al_wq^+5v#kw=8lw#)1i9h5qgq42
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2']
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1',
+                 '10.0.2.2',
+                 '165.232.79.200',
+                ]
 
 # For now, we are allowing all hosts. In production, we should specify your domain.
 CORS_ALLOW_ALL_ORIGINS = True
