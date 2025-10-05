cf1b65296a90c9f1cfc8db7ce1f2a075639bc57c
Ahmet <cck181851@gmail.com>
2025-10-05 15:10:22 +0300

    notifications page implementation

--- Web_Frontend/genfit_frontend/src/main.tsx
+++ Web_Frontend/genfit_frontend/src/main.tsx
@@ -6,8 +6,8 @@ import { queryClient } from './lib';
 import HomePage from "./pages/home/HomePage";
 import AuthPage from "./pages/auth/AuthPage";
 import GoalPage from "./pages/goal/GoalPage";
+import NotificationsPage from "./pages/notifications/notificationPage"; // Add this import
 import '../index.css';
-
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
           <Route path="/home" element={<HomePage />} />
           <Route path="/auth" element={<AuthPage />} />
           <Route path="/goals" element={<GoalPage />} />
+          <Route path="/notifications" element={<NotificationsPage />} /> {/* Add this route */}
         </Routes>
       </Router>
     </QueryClientProvider>
